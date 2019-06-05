34157c68d35c073a4f25825e0472241a5e87aac4
DESKTOP-4BGVB69\David <davidgiovannini90@gmail.com>
2019-06-05 19:16:03 -0300

    Archivos

---
+++ WebApp/Logica/ClasePrincipal.cs
@@ -0,0 +1,41 @@
+﻿using Contratos;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logica
+{
+    public class ClasePrincipal
+    {
+        List<Docente> ListaDocentes = new List<Docente>();
+        List<UsuarioLogueado> ListaUsuarios = new List<UsuarioLogueado>();
+        List<Padre> ListaPadres = new List<Padre>();
+        List<Hijo> ListaHijos = new List<Hijo>();
+
+        public Respuesta CrearArchivos()
+        {
+            Respuesta NuevaRespuesta = new Respuesta(true, "");
+
+            if (File.Exists(@"C:\Users\David\Desktop\David-TP Prog\Archivos\Docentes.txt"))
+            {
+                File.Create(@"C:\Users\David\Desktop\David-TP Prog\Archivos\Docentes.txt").Close();
+            }
+            if (File.Exists(@"C:\Users\David\Desktop\David-TP Prog\Archivos\Padres.txt"))
+            {
+                File.Create(@"C:\Users\David\Desktop\David-TP Prog\Archivos\Padres.txt").Close();
+            }
+            if (File.Exists(@"C:\Users\David\Desktop\David-TP Prog\Archivos\Hijos.txt"))
+            {
+                File.Create(@"C:\Users\David\Desktop\David-TP Prog\Archivos\Hijos.txt").Close();
+            }
+            if (File.Exists(@"C:\Users\David\Desktop\David-TP Prog\Archivos\Usuarios.txt"))
+            {
+                File.Create(@"C:\Users\David\Desktop\David-TP Prog\Archivos\Usuarios.txt").Close();
+            }
+            return NuevaRespuesta;
+        }
+    }
+}
---
+++ WebApp/Logica/Respuesta.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logica
+{
+    public class Respuesta
+    {
+        public bool Resp { get; set; }
+        public string Mensaje { get; set; }
+
+        public Respuesta()
+        {
+            this.Resp = true;
+        }
+
+        public Respuesta(bool respuesta, string mensaje)
+        {
+            this.Resp = respuesta;
+            this.Mensaje = mensaje;
+        }
+    }
+}
